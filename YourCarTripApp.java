43944e92bb1ae8e6f0da5464e185de4c5dd0471b
Genny Ocampo <gegex08@aol.com>
2024-02-16 22:20:00 -0600

    Update YourCarTripApp.java
    updated leg

--- YourCarTripApp.java
+++ YourCarTripApp.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package yourcartripapp;
 
 import java.util.Scanner;
@@ -12,60 +9,52 @@ import java.util.Scanner;
  */
 public class YourCarTripApp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
+        //declared variables
         char pointA = 'A';//starts at letter A
         double totalDistance = 0.0;//begins at 0 distance
         double Speed;//user will decide speed
         double totalTime = 0.0;// total time starts at 0
-        double gasUsed = 0.0;// used gas
-        
-        MyCar gas1 = new MyCar( 6.00 ); // create MyCar object that has gas starting at 6 gallons
-        MyCar odometer1 = new MyCar(60000.00);//create MyCar object that has initial odometer at 60000
-        
+        double gasUsed;//initial gas prior to planned trip
+                
         Scanner input = new Scanner(System.in);
-
+        
+        //Ask user input for planning a trip
         System.out.println("Are you going on a trip? (1 = Yes, 0 = No)");
-        int answer = input.nextInt();
+        double answer = input.nextDouble();
         
-        
+        //Ask user to input the amount of gallons prior to trip
+        System.out.println("How many gallons of gas are in your tank. (Int 1-15)");
+        gasUsed = input.nextDouble();
+                 
         while(answer == 1){//if user chooses 1 program will repeat
             
-            System.out.printf("Enter the amount of miles traveled at point %s: \n", pointA);
-            totalDistance = input.nextInt();
-            
-            System.out.println("How many gallons of gas are in your tank. (Int 1-15)");
-            gasUsed = input.nextInt();
-            
-            System.out.println("Enter speed");
-            Speed = input.nextInt();
+            System.out.printf("Enter the amount of miles traveled at point %s: \n", pointA);//starting at point A ask user how many miles were traveled
+            totalDistance = input.nextDouble();
+
+            System.out.println("Enter speed");//Ask user to input speed
+            Speed = input.nextDouble();
             
             
             //Ask if trip will be done again
-            System.out.println("Are you going on a trip? (1 = Yes, 0 = No)");
-            answer = input.nextInt();
+            System.out.println("Are you going on a trip? (1 = Yes, 0 = No)");//ask user if they want to add another leg to the trip
+            answer = input.nextDouble();
             
             //Equations
             totalDistance++;//Add distance to previous distance
             totalTime += totalDistance / Speed * 60;//calculates distance over time mph
-            gasUsed -= gasUsed;
-            
+            gasUsed -= totalDistance / 25;//the average car uses 25 gallons per mile
             
             //will iterate to the next point if user decides to add another trip
             pointA++;
         }
-        gasUsed++;
-        System.out.printf("The total distance traveled:\n", totalDistance);
-        System.out.println(totalTime);
-        System.out.printf("The total gas Used\n", gasUsed);
+        gasUsed++;//will iterate the gas equation for each time it asks user input
+        System.out.printf("You traveled about %.2f miles in about %.2f minutes.\n", totalDistance, totalTime);
+        MyCar leg1 = new MyCar(gasUsed ); // create MyCar object that has gas starting at user input
         
-        // display final numbers of each object
-        System.out.printf( "Total Gas Used: %.2f\n", gas1.getTotalFuel() );
-        System.out.printf( "Final Odometer Read: %.2f\n", odometer1.getTotalMiles() );
-        
-
+        // get method from object and alert us if we have enough gas to proceed
+        leg1.getTotalFuel();
 
     }
     
