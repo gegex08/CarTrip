f0d2e9fab81f9cfac46bd34f92cb4a00aaccd77d
Genny Ocampo <gegex08@aol.com>
2024-02-23 23:16:58 -0600

    Update YourCarTripApp.java

--- YourCarTripApp.java
+++ YourCarTripApp.java
@@ -1,5 +1,8 @@
-
-package yourcartripapp;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package daysinmonth;
 
 import java.util.Scanner;
 
@@ -7,55 +10,67 @@ import java.util.Scanner;
  *
  * @author geneivaocampo
  */
-public class YourCarTripApp {
+public class DaysInMonth {
 
-    public static void main(String[] args) {
-        
-        //declared variables
-        char pointA = 'A';//starts at letter A
-        double totalDistance = 0.0;//begins at 0 distance
-        double Speed;//user will decide speed
-        double totalTime = 0.0;// total time starts at 0
-        double gasUsed;//initial gas prior to planned trip
-                
-        Scanner input = new Scanner(System.in);
-        
-        //Ask user input for planning a trip
-        System.out.println("Are you going on a trip? (1 = Yes, 0 = No)");
-        double answer = input.nextDouble();
-        
-        //Ask user to input the amount of gallons prior to trip
-        System.out.println("How many gallons of gas are in your tank. (Int 1-15)");
-        gasUsed = input.nextDouble();
-                 
-        while(answer == 1){//if user chooses 1 program will repeat
-            
-            System.out.printf("Enter the amount of miles traveled at point %s: \n", pointA);//starting at point A ask user how many miles were traveled
-            totalDistance = input.nextDouble();
-
-            System.out.println("Enter speed");//Ask user to input speed
-            Speed = input.nextDouble();
-            
-            
-            //Ask if trip will be done again
-            System.out.println("Are you going on a trip? (1 = Yes, 0 = No)");//ask user if they want to add another leg to the trip
-            answer = input.nextDouble();
-            
-            //Equations
-            totalDistance++;//Add distance to previous distance
-            totalTime += totalDistance / Speed * 60;//calculates distance over time mph
-            gasUsed -= totalDistance / 25;//the average car uses 25 gallons per mile
-            
-            //will iterate to the next point if user decides to add another trip
-            pointA++;
-        }
-        gasUsed++;//will iterate the gas equation for each time it asks user input
-        System.out.printf("You traveled about %.2f miles in about %.2f minutes.\n", totalDistance, totalTime);
-        MyCar leg1 = new MyCar(gasUsed ); // create MyCar object that has gas starting at user input
-        
-        // get method from object and alert us if we have enough gas to proceed
-        leg1.getTotalFuel();
-
-    }
     
+  public static void main(String[] strings) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the year: ");// Ask User for year
+        int year = input.nextInt();
+        System.out.println("Enter the month (1-12): ");//Ask User for month
+        int month = input.nextInt();
+
+        switch (month) {
+            //All user inputs number equal to the case for 30 days
+            case 4:
+                System.out.println("April has 30 days");
+                break;
+            case 6:
+                System.out.println("June has 30 days");
+                break;
+            case 9:
+                System.out.println("September has 30 days");
+                break;
+            case 11:
+                System.out.println("November has 30 days");
+                break;
+            //All user inputs number equal to the case for 31 days
+            case 1:
+                System.out.println("January has 31 days");
+                break;
+            case 3:
+                System.out.println("March has 31 days");
+                break;
+            case 5:
+                System.out.println("May has 31 days");
+                break;
+            case 7:
+                System.out.println("July has 31 days");
+                break;
+            case 8:
+                System.out.println("August has 31 days");
+                break;
+            case 10:
+                System.out.println("October has 31 days");
+                break;
+            case 12:
+                System.out.println("December has 31 days");
+                break;
+            //month of february for each 4 years with the exection 
+            case 2:
+                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                    System.out.println("February has 29 days in year " + year);
+                } else {
+                    System.out.println("February has 28 days in year " + year);
+                }
+                break;
+            default:
+                System.out.println("Not a valid Month");
+        }
+  }
 }
+
+    
+
